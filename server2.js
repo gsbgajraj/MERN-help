97f5b87ce0984814475e789b9e078b83508380cc
Gajraj Singh Bhati <110357099+gsbgajraj@users.noreply.github.com>
2024-08-31 16:16:15 +0530

    Create server2.js , use it for mention build, dist path in server.js file

---
+++ server2.js
@@ -0,0 +1,10 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const buildPath = path.join(__dirname, '../frontend/dist');
+app.use(express.static(buildPath));
+
+// paste this in the last of the serever.js file do not include in the above, nothing will work, you already faced the error
+
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html')); });
